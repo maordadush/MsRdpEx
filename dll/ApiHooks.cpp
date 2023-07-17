0a08979b8c058fa5f7fe0532f16278f77ed80878
maordadush <maordadush@gmail.com>
2023-07-17 16:31:45 +0300

    WIP

--- dll/ApiHooks.cpp
+++ dll/ApiHooks.cpp
@@ -366,7 +366,10 @@ BOOL Hook_CredReadW(LPCWSTR TargetName, DWORD Type, DWORD Flags, PCREDENTIALW* C
     return success;
 }
 
+#include <bcrypt.h>
+#pragma comment(lib, "bcrypt.lib")
 #include <dpapi.h>
+#include <wincrypt.h>
 #pragma comment(lib, "crypt32.lib")
 
 BOOL(WINAPI* Real_CryptProtectMemory)(LPVOID pDataIn, DWORD cbDataIn, DWORD dwFlags) = CryptProtectMemory;
@@ -400,17 +403,28 @@ BOOL(WINAPI* Real_CryptProtectData)(DATA_BLOB* pDataIn, LPCWSTR szDataDescr, DAT
 BOOL(WINAPI* Real_CryptUnprotectData)(DATA_BLOB* pDataIn, LPWSTR* ppszDataDescr, DATA_BLOB* pOptionalEntropy,
     PVOID pvReserved, CRYPTPROTECT_PROMPTSTRUCT* pPromptStruct, DWORD dwFlags, DATA_BLOB* pDataOut) = CryptUnprotectData;
 
+NTSTATUS(WINAPI* Real_BCryptImportKeyPair)(BCRYPT_ALG_HANDLE hAlgorithm, BCRYPT_KEY_HANDLE hImportKey,
+    LPCWSTR pszBlobType, BCRYPT_KEY_HANDLE* phKey, PUCHAR pbInput, ULONG cbInput, ULONG dwFlags) = BCryptImportKeyPair;  
+
+NTSTATUS(WINAPI* Real_BCryptEncrypt)(BCRYPT_KEY_HANDLE hKey, PUCHAR pbInput,
+   ULONG cbInput, VOID* pPaddingInfo, PUCHAR pbIV, ULONG cbIV, PUCHAR pbOutput, ULONG cbOutput, ULONG* pcbResult, ULONG dwFlags) = BCryptEncrypt; 
+
+NTSTATUS(WINAPI* Real_BCryptImportKey)(BCRYPT_ALG_HANDLE hAlgorithm, BCRYPT_KEY_HANDLE hImportKey,
+    LPCWSTR pszBlobType, BCRYPT_KEY_HANDLE* phKey, PUCHAR pbKeyObject, ULONG cbKeyObject, PUCHAR pbInput, ULONG cbInput, ULONG dwFlags) = BCryptImportKey;     
+
+BOOL(WINAPI* Real_CryptImportPublicKeyInfoEx2)(DWORD dwCertEncodingType, PCERT_PUBLIC_KEY_INFO pInfo, DWORD dwFlags, void *pvAuxInfo, BCRYPT_KEY_HANDLE *phKey) = CryptImportPublicKeyInfoEx2;         
+
 BOOL Hook_CryptProtectData(DATA_BLOB* pDataIn, LPCWSTR szDataDescr, DATA_BLOB* pOptionalEntropy,
     PVOID pvReserved, CRYPTPROTECT_PROMPTSTRUCT* pPromptStruct, DWORD dwFlags, DATA_BLOB* pDataOut)
 {
     BOOL success;
 
-    MsRdpEx_LogPrint(DEBUG, "CryptProtectData(pDataIn->cbData=%d, dwFlags=%d):", pDataIn->cbData, dwFlags);
+    MsRdpEx_LogPrint(TRACE, "CryptProtectData(pDataIn->cbData=%d, dwFlags=%d):", pDataIn->cbData, dwFlags);
     MsRdpEx_LogDump(TRACE, (uint8_t*)pDataIn->pbData, (size_t)pDataIn->cbData);
 
     success = Real_CryptProtectData(pDataIn, szDataDescr, pOptionalEntropy, pvReserved, pPromptStruct, dwFlags, pDataOut);
 
-    MsRdpEx_LogPrint(DEBUG, "CryptProtectData(pDataOut->cbData=%d):", pDataOut->cbData);
+    MsRdpEx_LogPrint(TRACE, "CryptProtectData(pDataOut->cbData=%d):", pDataOut->cbData);
     MsRdpEx_LogDump(TRACE, (uint8_t*)pDataOut->pbData, (size_t)pDataOut->cbData);
 
     return success;
@@ -421,13 +435,76 @@ BOOL Hook_CryptUnprotectData(DATA_BLOB* pDataIn, LPWSTR* ppszDataDescr, DATA_BLO
 {
     BOOL success;
 
-    MsRdpEx_LogPrint(DEBUG, "CryptUnprotectData(pDataIn->cbData=%d, dwFlags=%d):", pDataIn->cbData, dwFlags);
+    MsRdpEx_LogPrint(TRACE, "CryptUnprotectData(pDataIn->cbData=%d, dwFlags=%d):", pDataIn->cbData, dwFlags);
     MsRdpEx_LogDump(TRACE, (uint8_t*)pDataIn->pbData, (size_t)pDataIn->cbData);
 
     success = Real_CryptUnprotectData(pDataIn, ppszDataDescr, pOptionalEntropy, pvReserved, pPromptStruct, dwFlags, pDataOut);
 
-    MsRdpEx_LogPrint(DEBUG, "CryptUnprotectData(pDataOut->cbData=%d):", pDataOut->cbData);
+    MsRdpEx_LogPrint(TRACE, "CryptUnprotectData(pDataOut->cbData=%d):", pDataOut->cbData);
     MsRdpEx_LogDump(TRACE, (uint8_t*)pDataOut->pbData, (size_t)pDataOut->cbData);
+
+    return success;
+}
+
+NTSTATUS Hook_BCryptImportKeyPair(BCRYPT_ALG_HANDLE hAlgorithm, BCRYPT_KEY_HANDLE hImportKey,
+    LPCWSTR pszBlobType, BCRYPT_KEY_HANDLE* phKey, PUCHAR pbInput, ULONG cbInput, ULONG dwFlags)
+{
+    NTSTATUS success;
+
+    MsRdpEx_LogPrint(TRACE, "BCryptImportKeyPair(pbInput=%s, cbInput=%d):", pbInput, cbInput);
+    MsRdpEx_LogDump(TRACE, (uint8_t*)pbInput, (size_t)cbInput);
+
+    success = Real_BCryptImportKeyPair(hAlgorithm, hImportKey, pszBlobType, phKey, pbInput, cbInput, dwFlags);
+
+    // MsRdpEx_LogPrint(DEBUG, "CryptUnprotectData(pDataOut->cbData=%d):", pDataOut->cbData);
+    // MsRdpEx_LogDump(TRACE, (uint8_t*)pDataOut->pbData, (size_t)cbInput);
+
+    return success;
+}
+
+NTSTATUS Hook_BCryptEncrypt(BCRYPT_KEY_HANDLE hKey, PUCHAR pbInput,
+   ULONG cbInput, VOID* pPaddingInfo, PUCHAR pbIV, ULONG cbIV, PUCHAR pbOutput, ULONG cbOutput, ULONG* pcbResult, ULONG dwFlags)
+{
+    NTSTATUS success;
+
+    MsRdpEx_LogPrint(TRACE, "BCryptEncrypt(pbInput=%s, cbInput=%d):", pbInput, cbInput);
+    MsRdpEx_LogDump(TRACE, (uint8_t*)pbInput, (size_t)cbInput);
+
+    success = Real_BCryptEncrypt(hKey, pbInput, cbInput, pPaddingInfo, pbIV, cbIV, pbOutput, cbOutput, pcbResult, dwFlags);
+
+    // MsRdpEx_LogPrint(DEBUG, "CryptUnprotectData(pDataOut->cbData=%d):", pDataOut->cbData);
+    // MsRdpEx_LogDump(TRACE, (uint8_t*)pDataOut->pbData, (size_t)cbInput);
+
+    return success;
+}
+
+NTSTATUS Hook_BCryptImportKey(BCRYPT_ALG_HANDLE hAlgorithm, BCRYPT_KEY_HANDLE hImportKey,
+    LPCWSTR pszBlobType, BCRYPT_KEY_HANDLE* phKey, PUCHAR pbKeyObject, ULONG cbKeyObject, PUCHAR pbInput, ULONG cbInput, ULONG dwFlags)
+{
+    NTSTATUS success;
+
+    MsRdpEx_LogPrint(TRACE, "BCryptImportKey(pbInput=%s, cbInput=%d):", pbInput, cbInput);
+    MsRdpEx_LogDump(TRACE, (uint8_t*)pbInput, (size_t)cbInput);
+
+    success = Real_BCryptImportKey(hAlgorithm, hImportKey, pszBlobType, phKey, pbKeyObject, cbKeyObject, pbInput, cbInput, dwFlags);
+
+    // MsRdpEx_LogPrint(DEBUG, "CryptUnprotectData(pDataOut->cbData=%d):", pDataOut->cbData);
+    // MsRdpEx_LogDump(TRACE, (uint8_t*)pDataOut->pbData, (size_t)cbInput);
+
+    return success;
+}
+
+BOOL Hook_CryptImportPublicKeyInfoEx2(DWORD dwCertEncodingType, PCERT_PUBLIC_KEY_INFO pInfo, DWORD dwFlags, void *pvAuxInfo, BCRYPT_KEY_HANDLE *phKey)
+{
+    BOOL success;
+
+    MsRdpEx_LogPrint(TRACE, "CryptImportPublicKeyInfoEx2(dwCertEncodingType=%d, pInfo=%d):", dwCertEncodingType, pInfo->PublicKey);
+    MsRdpEx_LogDump(TRACE, (uint8_t*)pInfo->PublicKey.pbData, (size_t)pInfo->PublicKey.cbData);
+
+    success = Real_CryptImportPublicKeyInfoEx2(dwCertEncodingType, pInfo, dwFlags, pvAuxInfo, phKey);
+
+    // MsRdpEx_LogPrint(DEBUG, "CryptUnprotectData(pDataOut->cbData=%d):", pDataOut->cbData);
+    // MsRdpEx_LogDump(TRACE, (uint8_t*)pDataOut->pbData, (size_t)cbInput);
 
     return success;
 }
@@ -496,17 +573,21 @@ LONG MsRdpEx_AttachHooks()
     DetourUpdateThread(GetCurrentThread());
     MSRDPEX_DETOUR_ATTACH(Real_LoadLibraryA, Hook_LoadLibraryA);
     MSRDPEX_DETOUR_ATTACH(Real_LoadLibraryW, Hook_LoadLibraryW);
-    //MSRDPEX_DETOUR_ATTACH(Real_GetProcAddress, Hook_GetProcAddress);
-    //MSRDPEX_DETOUR_ATTACH(Real_GetAddrInfoW, Hook_GetAddrInfoW);
-    //MSRDPEX_DETOUR_ATTACH(Real_GetAddrInfoExW, Hook_GetAddrInfoExW);
+    MSRDPEX_DETOUR_ATTACH(Real_GetProcAddress, Hook_GetProcAddress);
+    MSRDPEX_DETOUR_ATTACH(Real_GetAddrInfoW, Hook_GetAddrInfoW);
+    MSRDPEX_DETOUR_ATTACH(Real_GetAddrInfoExW, Hook_GetAddrInfoExW);
     MSRDPEX_DETOUR_ATTACH(Real_BitBlt, Hook_BitBlt);
     MSRDPEX_DETOUR_ATTACH(Real_StretchBlt, Hook_StretchBlt);
     MSRDPEX_DETOUR_ATTACH(Real_RegisterClassExW, Hook_RegisterClassExW);
     MSRDPEX_DETOUR_ATTACH(Real_CredReadW, Hook_CredReadW);
-    //MSRDPEX_DETOUR_ATTACH(Real_CryptProtectMemory, Hook_CryptProtectMemory);
-    //MSRDPEX_DETOUR_ATTACH(Real_CryptUnprotectMemory, Hook_CryptUnprotectMemory);
-    //MSRDPEX_DETOUR_ATTACH(Real_CryptProtectData, Hook_CryptProtectData);
-    //MSRDPEX_DETOUR_ATTACH(Real_CryptUnprotectData, Hook_CryptUnprotectData);
+    // MSRDPEX_DETOUR_ATTACH(Real_CryptProtectMemory, Hook_CryptProtectMemory);
+    // MSRDPEX_DETOUR_ATTACH(Real_CryptUnprotectMemory, Hook_CryptUnprotectMemory);
+    // MSRDPEX_DETOUR_ATTACH(Real_CryptProtectData, Hook_CryptProtectData);
+    // MSRDPEX_DETOUR_ATTACH(Real_CryptUnprotectData, Hook_CryptUnprotectData);
+    MSRDPEX_DETOUR_ATTACH(Real_BCryptEncrypt, Hook_BCryptEncrypt);
+    MSRDPEX_DETOUR_ATTACH(Real_BCryptImportKey, Hook_BCryptImportKey);
+    MSRDPEX_DETOUR_ATTACH(Real_BCryptImportKeyPair, Hook_BCryptImportKeyPair);
+    MSRDPEX_DETOUR_ATTACH(Real_CryptImportPublicKeyInfoEx2, Hook_CryptImportPublicKeyInfoEx2);
     MsRdpEx_AttachSspiHooks();
     error = DetourTransactionCommit();
 
@@ -535,17 +616,21 @@ LONG MsRdpEx_DetachHooks()
     DetourUpdateThread(GetCurrentThread());
     MSRDPEX_DETOUR_DETACH(Real_LoadLibraryA, Hook_LoadLibraryA);
     MSRDPEX_DETOUR_DETACH(Real_LoadLibraryW, Hook_LoadLibraryW);
-    //MSRDPEX_DETOUR_DETACH(Real_GetProcAddress, Hook_GetProcAddress);
-    //MSRDPEX_DETOUR_DETACH(Real_GetAddrInfoW, Hook_GetAddrInfoW);
-    //MSRDPEX_DETOUR_DETACH(Real_GetAddrInfoExW, Hook_GetAddrInfoExW);
+    MSRDPEX_DETOUR_DETACH(Real_GetProcAddress, Hook_GetProcAddress);
+    MSRDPEX_DETOUR_DETACH(Real_GetAddrInfoW, Hook_GetAddrInfoW);
+    MSRDPEX_DETOUR_DETACH(Real_GetAddrInfoExW, Hook_GetAddrInfoExW);
     MSRDPEX_DETOUR_DETACH(Real_BitBlt, Hook_BitBlt);
     MSRDPEX_DETOUR_DETACH(Real_StretchBlt, Hook_StretchBlt);
     MSRDPEX_DETOUR_DETACH(Real_RegisterClassExW, Hook_RegisterClassExW);
     MSRDPEX_DETOUR_DETACH(Real_CredReadW, Hook_CredReadW);
-    //MSRDPEX_DETOUR_DETACH(Real_CryptProtectMemory, Hook_CryptProtectMemory);
-    //MSRDPEX_DETOUR_DETACH(Real_CryptUnprotectMemory, Hook_CryptUnprotectMemory);
-    //MSRDPEX_DETOUR_DETACH(Real_CryptProtectData, Hook_CryptProtectData);
-    //MSRDPEX_DETOUR_DETACH(Real_CryptUnprotectData, Hook_CryptUnprotectData);
+    // MSRDPEX_DETOUR_DETACH(Real_CryptProtectMemory, Hook_CryptProtectMemory);
+    // MSRDPEX_DETOUR_DETACH(Real_CryptUnprotectMemory, Hook_CryptUnprotectMemory);
+    // MSRDPEX_DETOUR_DETACH(Real_CryptProtectData, Hook_CryptProtectData);
+    // MSRDPEX_DETOUR_DETACH(Real_CryptUnprotectData, Hook_CryptUnprotectData);
+    MSRDPEX_DETOUR_DETACH(Real_BCryptEncrypt, Hook_BCryptEncrypt);
+    MSRDPEX_DETOUR_DETACH(Real_BCryptImportKey, Hook_BCryptImportKey);
+    MSRDPEX_DETOUR_DETACH(Real_BCryptImportKeyPair, Hook_BCryptImportKeyPair);
+    MSRDPEX_DETOUR_DETACH(Real_CryptImportPublicKeyInfoEx2, Hook_CryptImportPublicKeyInfoEx2);
     MsRdpEx_DetachSspiHooks();
     error = DetourTransactionCommit();
 
